8739f4d2e74ba54f4ce25372730cf3fcc92f89ff
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-25 12:00:38 -0400

    ironcursor

--- Index.js
+++ Index.js
@@ -39,11 +39,6 @@ const netheriteCursorPrice=document.querySelector("#netheriteCursorPrice")
 const dpscounter=document.querySelector("#dpscount")
 const stage=document.querySelector("#boss")
 const damage=document.querySelector("#walter")
-const music = new Audio('./Images/minecraftSong.wav');
-function music(){
-  music.play();
-  music.loop = true;
-}
 
 let clicks = 0
 let woodSwordCost = 50
